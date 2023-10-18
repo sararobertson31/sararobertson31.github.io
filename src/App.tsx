0d85957c54fb7b658f6e8eb7b786fbdf0a788d7f
jacobhanson <jhanreg11@gmail.com>
2023-10-17 22:34:54 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <div className="landing-container">
+          <h1>Sara Robertson</h1>
+        </div>
+        <h2>Page 1</h2>
+        <h2>Page 2</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
