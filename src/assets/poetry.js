a91605acf9af95dd20b22459f8d283f73a925f63
sararobertson31 <148172832+sararobertson31@users.noreply.github.com>
2023-12-18 22:07:40 -0800

    Update poetry.js

--- src/assets/poetry.js
+++ src/assets/poetry.js
@@ -44,20 +44,199 @@ const stories = [{
     and make angels in the dirt.
     `,
     imageFile: "headshot.webp"
-},
-{
-    title: "BLACH",
-    content: `BLACH
-        COCK
-        SOCK`,
-    imageFile: "headshot.webp"
-},
-{
-    title: "BLACH",
-    content: `BLACH
-        COCK
-        SOCK`,
-    imageFile: "headshot.webp"
-}];
+},{
+    title: `animal/vegetable/mineral`,
+    content: `It goes like this:
+Upon a seam skin can be peeled back / to reveal muscle, which, when peeled back / clings to bone / inserted into pockets / held together by / muscle pulling bone like / gears pushing levers into place / webby veins tangled all over one another to / 
+hardwire this all into		something electric.
+ 
+Upon a seam skin can be peeled back / to reveal webs of layered wet flesh / do you see yet? / sweep the dirt from their eyelashes / pull the balls of moss from their mouths / watch them grow all gray and green amongst spores.
+ 
+Upon a seam skin can be peeled back / to reveal meat clinging to bone, which, when peeled back / reveals hollow white.
+ 
+It goes like this:
+A body, splayed on a table / skin, peeled back upon a seam / to be put into buckets into compartments / as shoulder is to hip is / forearm exists to keep elbow from wrist as / inside the soft curve of a thigh is all red wet and veiny / 
+go deeper / 
+cold and calcified.
+ 
+I am all made up of calcium and iron /
+by muscle sprouting from tendon / clinging to bone
+buried under folds of skin as
+the throat folds over itself when it swallows.
+`,
+    imageFile: `headshot.webp`
+},{
+    title: `Functional Gravity`,
+    content: `It is the rainiest season now, after the dead cold of winter, I write:
+A little hyperbole never hurts.
+I am trying to make meaning out of the meaningless.
+For all the vapidity of the meaningless my vision gets a little blurry around the edges.
+I count the exponential blooming of branches
+as four extends from two extends
+in some mathematical way I think the equidistance between my steps
+equals that I may not exist here // I may exist here
+if I choose to take one step wide enough for two,
+if I picked a flower from my neighbor’s garden and put it in my pocket,
+will that be enough to incriminate me?
+If I stole a cigarette from a pack left on the table in the entryway,
+if I said numerous mildly ugly things,
+if today I had to stifle tears at the sight of a car stopping for a squirrel,
+and honking twice before it ran across the street.
+The boy walking in front of me turned around
+and we smiled at each other, 
+united in the witnessing of a shred of human tenderness,
+how desperate we are for it these days.
+The breeze picked up our laughter and carried it off like balloons.
+Do you know that I am sensitive?
+Everything around me is quiet:
+4-a.m. quiet, freshly-cut-grass quiet, refrigerator quiet, yellow-light-behind-a-curtained-window quiet, picking-raisins-out-of-carrot-cake quiet, you’re-My-Daughter-why-won’t-you-talk-to-me quiet, sheets-warm-from-the-dryer quiet, sitting-on-a-splitted-tree-stump quiet, dial-tone quiet, please-leave-a-message quiet, a-crawdad-drags-itself-through-the-muddy-creekbed quiet, the-number-you-are-trying-to-contact-cannot-be-reached quiet, want-to-go-to-the-beach quiet, pale-plastic-stars-glued-to-my-bedroom-ceiling quiet –
+You see, if I say that I am trying to write a poem that is self-aware,
+will that be enough to succeed?
+For what it’s worth, if I am able to take a step big enough to be two but it is actually only one,
+my feet are still tethered to the ground.
+`,
+    imageFile: `headshot.webp`
+}, {
+    title: `the phoenix burns, too`,
+    content: `few seem to consider
+that the phoenix
+burns, too.
+
+in a moment it is 
+consumed
+in flame.
+
+and then its feathers begin
+to float off like embers
+and smolder
+in midair.
+
+chunks of flesh peel off, 
+leaving raw red 
+beneath.
+
+muscle melts
+from bone,
+
+and its eyes drip like wax 
+down half-feathered cheeks.
+
+i can hear it screaming:	white hot.
+
+i hear it weep.
+
+i whimper
+for an end to this agony.
+
+they tell us it will rise again-- 
+`,
+    imageFile: `headshot.webp`
+}, {
+    title: `bird bones`,
+    content: `i started collecting bits of bird bones
+when i walk.
+i’ll come across a piece
+brushed across the sidewalk into the bushes
+and weedy overgrowth by
+the feet that walked it before me.
+i slip down to my ankles
+and delicately, delicately
+pick it up -
+and into my pocket it goes.
+
+i don’t know when I began.
+
+i find pieces of all shapes and sizes.
+some are as sharp as a knife,
+and could have been a dagger.
+some are shards thinner than glass,
+some no larger than a grain of sand.
+but they are also brittle -
+
+once, part of a stretching wingspan
+or a skinny ankle,
+once in my pocket,
+eventually 
+become nothing but dust.
+`,
+    imageFile: `headshot.webp`,
+}, {
+    title: `What I do`,
+    subtitle: `march 2021`,
+    content: `I garden, and I feel the cool soil settle around my fingers as I place a naked root ball into the hole I have dug, and then I pat the earth down around it where it will be warm and safe. I water the plant to settle it in its new home, and I watch bubbles form on the ground as water flows through any pockets of air that remain beneath the earth, and the soil turns dark and rich.
+
+I cook, and I wonder if I’ll ever stop chopping garlic, but then when I toss it in the hot pan and the room fills with its rich sweet smell I decide that I would chop garlic every day for this. I rush to my garden and pluck the biggest, greenest basil leaves and rip them into teenytiny pieces as I rush back to the kitchen, my nose keen for the smell of burning.
+
+I drive with the windows down and my music loud (too loud) and I sing along, straining my voice to hear it over the music and then I get embarrassed because what if somebody heard me? But then I remind myself that I’m supposed not to care because almost nothing makes me feel more alive. 
+but I still turn my music down a little bit.
+
+I read, and my mind fills with legends of grandeur and heroism, knights defeating dragons, you know the jist–I am in places I’ve never been and seeing things I’ve never seen, and my eyes hang onto every last word, my belly fills with bubbly hope and I catch a fleeting taste of why I might be here. 
+
+I write, and the words fly off my pen and onto my page like it has a mind of its own. And other times, the blank white of the page resembles Chaos, that place where nothing is, nothing was, and nothing ever will be–and I chew on the end of my pen and leave teeth-marks until I remember my dentist’s warning.
+
+I brew coffee every morning before I do anything else, and I watch the beans ground up into dust and the dark caramel liquid drip-drip-drip into my empty mug. And I close my eyes while I breathe in that warm earthy smell in a moment of perfect peace, where everything is still and quiet. Fingers of steam reach up for my nose as I take that first sip.
+
+I punch in numbers on an old cash register (it’s one of these where you have to ring everything in by the numbers: 2-4-9-9 send, ding!) and sometimes my fingers fly over the keys so quickly that they type in some number combination never before seen to man and the register lets out a scream DIIIIIINGGG!!!! until I cancel it out. And I teach people how to use fertilizer and what soil to use for that plant and how tall that tree grows 
+and then someone asks me a question that I don’t know the answer to and my heart free-falls into my stomach.
+
+I go on walks when I can’t focus, and the cars whiz past me on the busy street, but I know I am protected from them by the impenetrable invisible boundary that separates sidewalk from street.
+And I wonder what would happen if I just kept walking, if I didn’t turn around this time, how far I would go and where I might end up.
+And the city mandated trees I walk past have scars where their too-low branches have been amputated. The new growth forms a raised ring over the old scar, and it looks like the tree is covered in eyes that watch at me as I walk past.
+
+I hike. My feet are bundled up in clunky boots and I breathe in the smell of oak and pine carried by the soft wind that cradles their leaves, and I glance back at my city, now the size of child’s play set down in the valley below, now shrinking into a pushpin on a map with each step. I listen to the twinkling birdsong and notice the way the sunlight is tinted green as it shines through the trees, and I have never felt more strong, and I have never felt more free.
+
+I try to sleep but it takes me a long time. I try to count sheep but then I forget and I turn over, and then I try to map my body from my toes to my head but then I lose my place and I turn over, then somewhere between breathing in for four and out for six and wondering why did I say that today? that was so stupid my mind finally slips into unconsciousness.
+
+I wake up and I look at the ceiling. I tell myself that I’ll get up when I count to five. One two three four five. I count to five again. I’ll get up this time. I count to five again. Again.
+
+Sometime soon I’d like to climb a tree again. Even now I can feel its cool, solid skin under my hands, my long hair getting tangled in sappy pine needles, I can hear the sharp hiss I’ll pull in through my teeth when my foot slips and my knee scrapes against the rough bark. I have almost forgotten what it’s like to sit on the slimmest branch that can hold my weight, my two legs dangling over either side and my arms wrapped tightly around the trunk in a hug, my face and chest pressed up against it. Our hearts beat in rhythm. A steady stream of blood oozes from my knee now, but I’ll patch it up when I get home. The wound will scab over, and later scar, then I’ll wear it proudly, like a medal of honor.
+`,
+}, {
+    title: `On Forests and Femininity`,
+    content: `everytime I shave my legs, it just grows back again
+and I just keep shearing it off
+
+But what if we imagined 
+that the thick black hairs that poke up through our skin were vast forests:
+a blanket of shade over mossy soil,
+cooling our burning earth,
+
+because we’ve all heard it said that deforestation to build new homes is 
+killing our planet.
+`,
+    imageFile: `headshot.webp`,
+}, {
+    title: `August 2020`,
+    content: `i will never know
+what she looked like,
+when my grandmother lay flat on the hospital bed,
+in 108 degree heat
+in august,
+
+so i imagine:
+fluorescent light beat down on her thin frame
+half-sunk in a flimsy mattress and paper sheets,
+buried under miles and		miles
+of tubes
+stuck in her arms,
+her nose, pumping food to her stomach
+her throat, pumping air to her
+bleeding lungs.
+i imagine that the air conditioner 
+a breathless whirring in her hospital room,
+no relief to doctors and nurses
+burdened under layers and 	layers 
+of personal protective equipment
+my 	grandmother
+just another face,	another bed.
+
+while outside the hospital,
+wildfires blew through our city
+and i could see heat waves floating off the pavement,
+blurring out the ground i stood on.
+`,
+    imageFile: `headshot.webp',
+}]
+    
 
 export default stories;
